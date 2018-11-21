6f922198dca0aa2e6f5fa12d1ee4310a2f432d79
jgabaja <jgabaja@gmail.com>
2018-11-21 09:20:08 +0800

    wip

--- project/settings.py
+++ project/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
