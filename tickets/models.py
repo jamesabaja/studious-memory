55cf0b80540dce2e279882b435f802e6dc442d4a
jgabaja <jgabaja@gmail.com>
2018-11-25 15:23:38 +0800

    added models

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,92 @@
 from django.db import models
 
 # Create your models here.
+
+class Passenger(models.Model):
+  userID = models.CharField(primary_key=True)
+  displayName = models.CharField()
+
+  def __str__(self):
+    return "%s (%s)" % (self.displayName, self.userID)
+
+class Bus(models.Model):
+  busID = models.CharField(primary_key=True)
+
+  def __str__(self):
+    return self.busID
+
+class Terminal(models.Model):
+  terminalID = models.CharField(primary_key=True)
+  terminalName = models.CharField()    
+
+  def __str__(self):
+    return "%s (%s)" % (self.terminalName, self.terminalID)
+
+class Driver(models.Model):
+  driverID = models.CharField(primary_key=True)
+  displayName = models.CharField()
+
+  def __str__(self):
+    return "%s (%s)" % (self.displayName, self.driverID)
+
+class Trip(models.Model): 
+  tripID = models.CharField(primary_key=True)
+  departureDate = models.DateField()
+  departureTime = models.TimeField()
+  sourceTerminal = models.OneToOneField(Terminal, on_delete=models.CASCADE)
+  destinationTerminal = models.OneToOneField(Terminal, on_delete=models.CASCADE)
+  seatsLeft = models.IntegerField()
+
+  def __str__(self):
+    return "%s -> %s (%s)" % (self.sourceTerminal, self.destinationTerminal, self.tripID)
+
+class Rating(models.Model):
+  userID = models.OneToOneField(Passenger, on_delete=models.CASCADE)
+  tripID = models.OneToOneField(Trip, on_delete=models.CASCADE)
+  rating = models.IntegerField()
+
+  class Meta:
+    unique_together = (('userID', 'tripID'),)
+
+  def __str__(self):
+    return '%s (%s)' % (self.tripID, self.userID)
+ 
+class Booking(models.Model):
+  userID = models.OneToOneField(Passenger, on_delete=models.CASCADE)
+  tripID = models.OneToOneField(Trip, on_delete=models.CASCADE)
+
+  class Meta:
+    unique_together = (('userID', 'tripID'),)
+
+  def __str__(self):
+    return '%s (%s)' % (self.tripID, self.userID)
+
+class Bus_Trip(models.Model):
+  busID = models.OneToOneField(Bus, on_delete=models.CASCADE)
+  tripID = models.OneToOneField(Trip, on_delete=models.CASCADE)
+
+  class Meta:
+    unique_together = (('busID', 'tripID'),)
+
+  def __str__(self):
+    return '%s (%s)' % (self.tripID, self.busID)
+
+class Bus_Driver(models.Model):
+  busID = models.OneToOneField(Bus, on_delete=models.CASCADE)
+  driverID = models.OneToOneField(Driver, on_delete=models.CASCADE)
+
+  class Meta:
+    unique_together = (('busID', 'driverID'),)
+
+  def __str__(self):
+    return '%s (%s)' % (self.driverID, self.busID)
+
+class CurrentTerminal(models.Model):
+  terminalID = models.OneToOneField(Terminal, on_delete=models.CASCADE)
+  tripID = models.OneToOneField(Trip, on_delete=models.CASCADE)
+
+  class Meta:
+    unique_together = (('terminalID', 'tripID'),)
+
+  def __str__(self):
+    return '%s (%s)' % (self.tripID, self.terminalID)
